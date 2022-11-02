4a24c33119e3750d0af3a501637f465c1f1e48f4
hsb-tonmoy <hsb.tonmoy@gmail.com>
2022-11-02 00:07:00 -0400

    added session serializers

--- apps/mock_visa_interview/serializers.py
+++ apps/mock_visa_interview/serializers.py
@@ -8,13 +8,30 @@ class MockVisaInterviewQuestionSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class MockVisaInterviewSessionSerializer(serializers.ModelSerializer):
+class MockVisaInterviewAnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MockVisaInterviewAnswer
+        fields = "__all__"
+
+
+class MockVisaInterviewAnswerRetrieveSerializer(serializers.ModelSerializer):
+    question = MockVisaInterviewQuestionSerializer(read_only=True)
+
+    class Meta:
+        model = MockVisaInterviewAnswer
+        fields = "__all__"
+
+
+class MockVisaInterviewSessionListSerializer(serializers.ModelSerializer):
     class Meta:
         model = MockVisaInterviewSession
         fields = "__all__"
 
 
-class MockVisaInterviewAnswerSerializer(serializers.ModelSerializer):
+class MockVisaInterviewSessionRetrieveSerializer(serializers.ModelSerializer):
+    mock_visa_interview_answers = MockVisaInterviewAnswerRetrieveSerializer(
+        many=True, read_only=True)
+
     class Meta:
-        model = MockVisaInterviewAnswer
+        model = MockVisaInterviewSession
         fields = "__all__"
--- apps/mock_visa_interview/views.py
+++ apps/mock_visa_interview/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from apps.mock_visa_interview.models import MockVisaInterviewAnswer, MockVisaInterviewQuestion, MockVisaInterviewSession
-from apps.mock_visa_interview.serializers import MockVisaInterviewAnswerSerializer, MockVisaInterviewQuestionSerializer, MockVisaInterviewSessionSerializer
+from apps.mock_visa_interview.serializers import MockVisaInterviewAnswerSerializer, MockVisaInterviewQuestionSerializer, MockVisaInterviewSessionListSerializer, MockVisaInterviewSessionRetrieveSerializer
 
 
 class MockVisaInterviewAnswerViewset(viewsets.ModelViewSet):
@@ -16,6 +16,15 @@ class MockVisaInterviewQuestionViewset(viewsets.ModelViewSet):
 
 class MockVisaInterviewSessionViewset(viewsets.ModelViewSet):
     queryset = MockVisaInterviewSession.objects.all()
-    serializer_class = MockVisaInterviewSessionSerializer
+    serializer_class = MockVisaInterviewSessionListSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['user']
+    retrieve_serializer_class = MockVisaInterviewSessionRetrieveSerializer
+
+    def get_serializer_class(self):
+
+        if self.action == 'retrieve':
+            if hasattr(self, 'retrieve_serializer_class'):
+                return self.retrieve_serializer_class
+
+        return super(MockVisaInterviewSessionViewset, self).get_serializer_class()
