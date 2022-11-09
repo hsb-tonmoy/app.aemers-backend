e0f2bc9c97bb73d3e0957a0730b6eace6a1bc31b
hsb.tonmoy <hsb.tonmoy@gmail.com>
2022-11-09 14:50:06 -0500

    replaced path with bytes

--- apps/mock_visa_interview/tasks.py
+++ apps/mock_visa_interview/tasks.py
@@ -1,18 +1,21 @@
+import io
 import wave
 from .models import MockVisaInterviewAnswer
 
 
-def concatenate_audio(audio_clip_paths, output_path):
+def concatenate_audio(audio_clip_paths):
+    output = io.BytesIO()
     data = []
     for clip in audio_clip_paths:
         w = wave.open(clip, "rb")
         data.append([w.getparams(), w.readframes(w.getnframes())])
         w.close()
-    output = wave.open(output_path, "wb")
+    output = wave.open(output, "wb")
     output.setparams(data[0][0])
     for clip in data:
         output.writeframes(clip[1])
     output.close()
+    return output
 
 
 def save_full_recording(session):
@@ -22,7 +25,7 @@ def save_full_recording(session):
         if question_audio:
             audio_clip_paths.append(question_audio)
         audio_clip_paths.append(answer.answer)
-    output_path = f'mock_visa_interview/sessions/{session.id}/full_recording.wav'
-    concatenate_audio(audio_clip_paths, output_path)
-    session.full_recording = output_path
+    # output_path = f'mock_visa_interview/sessions/{session.id}/full_recording.wav'
+    full_recording = concatenate_audio(audio_clip_paths)
+    session.full_recording = full_recording
     session.save()
