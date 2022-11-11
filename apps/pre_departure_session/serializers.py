d855b03e2395002373266fd4e49547392767d7f2
hsb.tonmoy <hsb.tonmoy@gmail.com>
2022-11-11 16:55:09 -0500

    updated pre_dep serializer

--- apps/pre_departure_session/serializers.py
+++ apps/pre_departure_session/serializers.py
@@ -1,8 +1,13 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from apps.pre_departure_session.models import PreDepartureSession, Participant
 
 
 class PreDepartureSessionSerializer(ModelSerializer):
+    time = SerializerMethodField()
+
+    def get_time(self, obj):
+        return obj.time.strftime("%I:%M %p")
+
     class Meta:
         model = PreDepartureSession
         fields = "__all__"
